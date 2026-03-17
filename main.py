d0aef99d8ea9d35c7de3f1662b496fd5fe2604cb
mina-wageh-m <mina.wageh.marzouk@gmail.com>
2026-03-17 05:07:27 +0200

    fix vanna import

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from vanna.remote import VannaDefault
+import vanna
+from vanna.openai import OpenAI_Chat
+from vanna.chromadb import ChromaDB_VectorStore
+
+class MyVanna(ChromaDB_VectorStore, OpenAI_Chat):
+    def __init__(self, config=None):
+        ChromaDB_VectorStore.__init__(self, config=config)
+        OpenAI_Chat.__init__(self, config=config)
+
+vn = MyVanna(config={
+    'api_key': 'vn-071c62b7ef4e4fe38fa7ae09a631dbee',
+    'model': 'Virtu'
+})
 
 app = FastAPI()
-
-vn = VannaDefault(
-    model='virtu',
-    api_key='vn-071c62b7ef4e4fe38fa7ae09a631dbee'
-)
 
 class Question(BaseModel):
     question: str
